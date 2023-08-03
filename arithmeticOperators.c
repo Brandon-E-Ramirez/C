2a2ae962b93e805cca0693b54c61010973e3c7c1
Brandon-E-Ramirez <brandon.evaristo.ramirez@gmail.com>
2023-08-02 21:05:43 -0500

    arithmetic operators

--- arithmeticOperators.c
+++ arithmeticOperators.c
@@ -1,13 +1,24 @@
 //https://www.youtube.com/watch?v=87SH2Cn0s9A&list=WL&index=127&t=1516s
-
+//https://www.tutorialspoint.com/cprogramming/c_arithmetic_operators.htm
 #include <stdio.h>
 //"standard library"
 #include <stdlib.h>
 //string library, imports string methods
 #include <string.h>
+#include <math.h>
 
 int main(){
+/*
+add, sub, mul, div, mod, increment, decrement
+*/    
 
-    
+int a = 4, b = 5, c = 6, d = 7, e = 8;
+int add = a + b;
+int mul = add % c;
+float div = mul / d;
+float result = (div * e);
+
+char expr[] = "(((a + b) % c)/d)*e";
+printf("The result of the expression \"%s\" is: %f\n", expr, result);
     return 0;
 }
